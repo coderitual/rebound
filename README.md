147b934a5d1b1dfbc47af2e96a4a2e973055a0cb
coderitual <skowronkow@gmail.com>
2019-08-19 22:13:20 +0200

    Update build

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 My entry for the 2019 [js13k](https://js13kgames.com/) competition.
 
+Play here: http://bouncebackgame.surge.sh
+
 ## License
 
 The code is available under the MIT license.
--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -119,7 +119,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"src/index.js":[function(require,module,exports) {
 console.log('hello world');
-},{}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8426" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8594" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -322,5 +322,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel/src/builtins/hmr-runtime.js","src/index.js"], null)
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/index.js"], null)
 //# sourceMappingURL=/src.a2b27638.js.map
