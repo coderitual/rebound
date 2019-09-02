7d6b606ef19f25e60dde711293a7f65298ba518f
coderitual <skowronkow@gmail.com>
2019-09-02 23:23:58 +0200

    Add simple particles

---
+++ src/fx.js
@@ -0,0 +1,61 @@
+import process from '/lib/process';
+
+const particles = new Set();
+
+for (let i = 0; i < 200; i++) {
+  particles.add({
+    x: 0,
+    y: 0,
+    velx: 0,
+    vely: 0,
+    mass: 0,
+    r: 0,
+    alive: false,
+  });
+}
+
+function explode(x, y, r, amount) {
+  let selected = 0;
+  for (let p of particles) {
+    if (!p.alive) {
+      p.x = x;
+      p.y = y;
+      p.velx = -1 + Math.random() * 2;
+      p.vely = -1 + Math.random() * 2;
+      p.mass = 0.5 + Math.random() * 2;
+      p.r = 0.5 + Math.random() * r;
+      p.mass = 0.5 + Math.random() * 2;
+      p.alive = true;
+      selected++;
+      if (selected === amount) {
+        break;
+      }
+    }
+  }
+}
+
+function update() {
+  for (let p of particles) {
+    if (p.alive) {
+      p.x += p.velx / p.mass;
+      p.y += p.vely / p.mass;
+      p.r -= 0.1;
+      if (p.r < 0.1) {
+        p.alive = false;
+      }
+    }
+  }
+}
+
+function draw(ctx) {
+  ctx.fillStyle = '#FFEC27';
+  for (let p of particles) {
+    if (p.alive) {
+      ctx.beginPath();
+      ctx.arc(p.x, p.y, p.r, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+  }
+}
+
+export default { update, draw, explode };
--- src/game.js
+++ src/game.js
@@ -4,6 +4,7 @@ import spritesheet from '/lib/spritesheet';
 import map from '/lib/map';
 import camera from '/lib/camera';
 import cls from '/lib/cls';
+import fx from './fx';
 
 let vw = 0;
 let vh = 0;
@@ -32,11 +33,12 @@ export function init() {
   ctx = canvas.getContext('2d');
   ctx.imageSmoothingEnabled = false;
 
-  spritesheet.define('hero', 0, 0, 8, 8);
+  spritesheet.define('hero', 0, 3 * 8, 8, 8);
   spritesheet.define('title', 0, 8, 56, 16);
 
   process(update, render);
   start();
+  fx.explode(64, 64, 5, 50);
 }
 
 function update(dt) {
@@ -44,17 +46,18 @@ function update(dt) {
   y += dy;
   if (x + 8 === vw || x === 0) dx = -dx;
   if (y + 8 === vh || y === 0) dy = -dy;
+  fx.update();
 }
 
 function render() {
   cls(ctx);
 
   camera(ctx, -20, -20, Math.cos(time++ / 200) * 10, Math.cos(time++ / 200) + 2);
-
   map(ctx);
   spritesheet.draw(ctx, 'hero', x, y);
 
   camera(ctx);
+  fx.draw(ctx);
   spritesheet.draw(ctx, 'title', (128 - 56) / 2, 50);
   font.printOutline(
     ctx,
