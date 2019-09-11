82c8fb7cdf49e37d2d24e150fdbf458a29a443c9
coderitual <skowronkow@gmail.com>
2019-09-11 21:31:27 +0200

    Add army and base

--- src/level.js
+++ src/level.js
@@ -8,6 +8,7 @@ import fx from './fx';
 import shape from '/lib/shape';
 import input from '/lib/input';
 import army from './army';
+import base from './base';
 
 let time = 1;
 const cd = cooldown();
@@ -15,12 +16,34 @@ const cd = cooldown();
 function load() {
   input.init();
   army.init();
+  base.init();
+
+  base.add({
+    playerId: 0,
+    x: 110,
+    y: 64 - 5,
+    angle: 90,
+    targetAngle: 180,
+    power: 1,
+    targetPower: 0,
+  });
+
+  base.add({
+    playerId: 1,
+    x: 4,
+    y: 64 - 5,
+    angle: -90,
+    targetAngle: 0,
+    power: 1,
+    targetPower: 0,
+  });
 
   army.add({ x: 84, y: 64, count: 8, player: 1 });
 }
 
 function update(dt) {
   army.update(dt);
+  base.update(dt);
   fx.update();
   time += dt;
 }
@@ -30,6 +53,7 @@ function render(ctx) {
   camera(ctx, 0, 0);
   map(ctx);
   camera(ctx);
+  base.draw(ctx);
   army.draw(ctx);
   shape.drawGrid(ctx);
 }
