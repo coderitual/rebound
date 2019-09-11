e33fd708d384cf48a16c56f7834f5ca0345d45f9
coderitual <skowronkow@gmail.com>
2019-09-12 00:07:11 +0200

    Update events

--- src/events.js
+++ src/events.js
@@ -5,7 +5,7 @@ function on(name, callback) {
 }
 function trigger(name, ...params) {
   if (!events.has(name)) {
-    throw new Error(`There is no event "${name}" to be triggered.`);
+    return;
   }
   events.get(name).forEach(callback => callback(...params));
 }
