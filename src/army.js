0084d37b2bd4e3196893127994572684e0fef258
coderitual <skowronkow@gmail.com>
2019-09-12 22:39:28 +0200

    Remove army circle

--- src/army.js
+++ src/army.js
@@ -34,34 +34,10 @@ function update(dt) {
 
 function draw(ctx) {
   for (let army of armies) {
-    spritesheet.draw(
-      ctx,
-      spriteForPlayer(army.playerId, 'flag'),
-      army.x - 2,
-      army.y - 3
-    );
-
-    ctx.lineWidth = 1;
-
-    // cripsy dotted area
-    for (let i = 0; i < 8; i++) {
-      const x =
-        Math.round(Math.cos(((Math.PI * 2) / 8) * i + time) * config.range) +
-        army.x;
-      const y =
-        Math.round(Math.sin(((Math.PI * 2) / 8) * i + time) * config.range) +
-        army.y;
-
-      ctx.fillStyle = 'white';
-      ctx.fillRect(x, y, 1, 1);
-      ctx.fillStyle = 'black';
-      ctx.fillRect(x, y + 1, 1, 1);
-    }
-
     // soldiers
     for (let i = 0; i < 3; i++) {
-      const x = Math.round(Math.cos(((Math.PI * 2) / 3) * i) * 3) + army.x + 7;
-      const y = Math.round(Math.sin(((Math.PI * 2) / 3) * i) * 3) + army.y + 10;
+      const x = Math.round(Math.cos(((Math.PI * 2) / 3) * i) * 3) + army.x;
+      const y = Math.round(Math.sin(((Math.PI * 2) / 3) * i) * 3) + army.y;
 
       spritesheet.draw(
         ctx,
